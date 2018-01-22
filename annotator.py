5127f33b2b3419ae61320549262cc5a73babb0fe
DS <>
2018-01-22 16:19:10 -0600

    (annotator) fixed minor bug where saving wouldn't save the annotations for the current animal

--- annotator.py
+++ annotator.py
@@ -163,6 +163,7 @@ class DeathDayEvaluator:
         file_dialog.setAcceptMode(Qt.QFileDialog.AcceptSave)
         if file_dialog.exec_():     # Run dialog box and check for a good exit
             save_path = pathlib.Path(file_dialog.selectedFiles()[0])
+            self.record_labeled_positions()
             self.worm_info.to_csv(save_path.open('w'),sep='\t')
             print('file written to '+str(save_path))
     
